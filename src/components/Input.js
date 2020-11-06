a4dc043bd369273b82e000cca69f39de00f27a6c
Shawn Harrington <shawn.harrington2776@gmail.com>
2020-11-06 01:06:31 -0800

    Update Input.js

--- src/components/Input.js
+++ src/components/Input.js
@@ -15,7 +15,7 @@ STEP 0:
 
 STEP 1:
   Create a slice of state called 'inputValue' and its 'setInputValue' buddy.
-  We should ini tialize this state to the empty string.
+  We should initialize this state to the empty string.
 
 STEP 2:
   Make the color of the text be crimson if the length of 'inputValue' goes over ten.
@@ -37,36 +37,32 @@ STEP 6:
 import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Input() {
-  /* STEP 1 */
-  const [inputValue, setInputValue ] = useState('');
+  const [ inputValue, setInputValue ] = useState('')
 
   const changeInput = evt => {
     // When the input changes, its whole value can be found inside the event object.
     // Log out the synthetic event object 'evt' and see for yourself.
     const { value } = evt.target;
     setInputValue(value)
-
     /* STEP 4 */
-
   };
+
   const reset = () => {
-    /* STEP 5 */
     setInputValue('')
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    // color: 'royalblue', /* STEP 2 */
-    color: (inputValue.length <= 10 ? 'royalblue' : 'crimson')
+    color: inputValue.length <= 10 ? 'royalblue' : 'crimson'
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
-  <div id='output' style={style}>{inputValue}</div> {/* STEP 3 */}
+  <div id='output' style={style}>{inputValue.toUpperCase()}</div> {/* STEP 3 */}
       <div>
-        <input id='input' type='text' onChange={changeInput} /> {/* STEP 6 */}
+        <input id='input' type='text' onChange={changeInput} value={inputValue}/> {/* STEP 6 */}
         <button id='resetInput' onClick={reset}>Reset</button>
       </div>
     </div>
