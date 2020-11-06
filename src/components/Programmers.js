7d445b9f920445c1834d442302a42b852a67885d
Shawn Harrington <shawn.harrington2776@gmail.com>
2020-11-06 01:07:31 -0800

    Update Programmers.js

--- src/components/Programmers.js
+++ src/components/Programmers.js
@@ -25,10 +25,10 @@ export const listOfAwesome = [
 ];
 
 export default function Programmers() {
+  const [ devName, setDevName ] = useState(listOfAwesome)
+  const [ devID, setDevID ] = useState(null)
   // We'll have to use the state hook twice, as we need two slices of state.
   // The programmers list on the one hand, and the id of the featured programmer on the other.
-  const [ devNames, setDevNames ] = useState(listOfAwesome);
-  const [ devIDs, setDevIDs ] = useState(listOfAwesome);
 
   const getNameOfFeatured = () => {
     // Leave this for last!
@@ -36,14 +36,15 @@ export default function Programmers() {
     // It's going to utilize both slices of state to return the _name_ of the featured dev.
     // The beauty of closures is that we can "see" both slices of state from this region
     // of the program, without needing to inject the information through arguments.
-    setDevIDs('1')
+    return (devName[devID - 1].name)
+
+
   };
 
   const style = {
     fontSize: '1.5em',
     marginTop: '0.5em',
-    // color: 'royalblue', // 🤔 color turns to gold, when celebrating
-    color: devIDs === null ? 'royalblue' : 'crimson'
+    color: devID !== null ? 'gold' : 'royalblue'
   };
 
   return (
@@ -54,9 +55,9 @@ export default function Programmers() {
           /* Nasty bug! We should map over a slice of state, instead of 'listOfAwesome'.
           We might think: "it works, though!" But if the list of programmers is not state,
           we could never add or edit programmers in the future. The list would be a static thing." */
-          devNames.map(dev =>
+          devName.map(dev =>
             <div className='programmer' key={dev.id}>
-              {dev.name} <button onClick={() => { devIDs }}>Feature</button>
+              {dev.name} <button onClick={() => { setDevID(dev.id) }}>Feature</button>
             </div>
           )
         }
@@ -66,7 +67,7 @@ export default function Programmers() {
           // Ternaries are fantastic to render "one thing or the other" depending on the "truthiness" of something.
           // Pseudo-code: if the currently featured id is truthy render text 1, otherwise render text 2.
           // Replace the hard-coded false with the correct variable.
-          devIDs === null
+          devID !== null
             ? `🎉 Let's celebrate ${getNameOfFeatured()}! 🥳`
             : 'Pick an awesome programmer'
         }
